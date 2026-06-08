97d97cc493b8fcd64fe30df6b2d8c276e1134a56
DEviL AngEL <25114fw021@gmail.com>
2026-06-08 13:33:51 +0530

    Day-8 uploaded

---
+++ Day-8/ques31.c
@@ -0,0 +1,19 @@
+//print character triangle
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    char ch;
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        ch='A';
+        for (j=1;j<=i;j++)
+        {
+            printf ("%c ",ch++);
+        }
+        printf("\n");
+    }
+    return 0;
+}
