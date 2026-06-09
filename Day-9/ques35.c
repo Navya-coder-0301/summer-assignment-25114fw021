29b84e451338b608a9811ae054cfbfbf79442a22
DEviL AngEL <25114fw021@gmail.com>
2026-06-09 12:32:45 +0530

    Day-9 uploaded

---
+++ Day-9/ques35.c
@@ -0,0 +1,20 @@
+//print repeated character pattern triangle
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    char ch='A';
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        for (j=1;j<=i;j++)
+        {
+            printf ("%c ",ch);
+        }
+        printf("\n");
+        ch++;
+
+    }
+    return 0;
+}
